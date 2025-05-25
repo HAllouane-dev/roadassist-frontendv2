3a38940d511dd66d168eef17cd119ca757c65229
HAllouane <allouanehatimfr@gmail.com>
2025-05-25 02:07:38 +0200

    feat(operator-dashboard): Replace mission list with Grafana dashboard component in operator dashboard

--- src/app/features/operator/pages/operator-dashboard/operator-dashboard.component.ts
+++ src/app/features/operator/pages/operator-dashboard/operator-dashboard.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { MissionDataGridComponent } from '../../../missions/components/mission-list/mission-list.component';
+import { GrafanaDashboardComponent } from '../../../../shared/components/grafana-dashboard/grafana-dashboard.component';
 
 @Component({
     selector: 'app-operator-dashboard',
     standalone: true,
-    imports: [MissionDataGridComponent],
-    template: `
-        <app-mission-list></app-mission-list>
-        <div>
-            <iframe
-                src="http://localhost:3000/d-solo/39cf0f8c-ed0e-431c-b938-9009cbaddbe1/mission-dashboard?orgId=1&from=1747501051254&to=1747522651254&timezone=browser&panelId=1&__feature.dashboardSceneSolo"
-                width="450"
-                height="200"
-                frameborder="0"
-            ></iframe>
-        </div>
-    `
+    template: ` <app-grafana-dashboard></app-grafana-dashboard> `,
+    imports: [GrafanaDashboardComponent]
 })
 export class OperatorDashboardComponent {}
